275b9199f0055e90eb3d5038dd3fbf54b1ea5ec8
Sunil Sharma <ssharmaca494@gmail.com>
2018-10-08 13:17:48 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-angular-website';
+  pageTitle = 'Simple Angular Website';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { AboutUsComponent } from './about/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    WelcomeComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutUsComponent },
+      {path: 'products', component: ProductListComponent },
+      {path: 'welcome', component: WelcomeComponent },
+      {path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])  
   ],
   providers: [],
   bootstrap: [AppComponent]
